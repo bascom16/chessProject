9d78f3b506ca5a40d7fb81ee25280d4f310fc746
bascom16 <bascomb1@byu.edu>
2025-02-11 19:36:47 -0700

    Removed duplicates in KnightMoveCalculator

--- shared/src/main/java/chess/calculator/KnightMoveCalculator.java
+++ shared/src/main/java/chess/calculator/KnightMoveCalculator.java
@@ -15,18 +15,18 @@ public class KnightMoveCalculator implements PieceMoveCalculator {
         int myCol = position.getColumn();
         int[] singleShift = {1, -1};
         int[] doubleShift = {2, -2};
-        for (int i : singleShift) {
-            for (int j : doubleShift) {
-                ChessPosition endPosition = new ChessPosition(myRow + i, myCol + j);
+        for (int iVertical : singleShift) {
+            for (int jVertical : doubleShift) {
+                ChessPosition endPosition = new ChessPosition(myRow + iVertical, myCol + jVertical);
                 ChessMove nextMove = new ChessMove(position, endPosition, null);
                 if (checkMove(board, nextMove)) {
                     moveList.add(nextMove);
                 }
             }
         }
-        for (int i : doubleShift) {
-            for (int j : singleShift) {
-                ChessPosition endPosition = new ChessPosition(myRow + i, myCol + j);
+        for (int iHorizontal : doubleShift) {
+            for (int jHorizontal : singleShift) {
+                ChessPosition endPosition = new ChessPosition(myRow + iHorizontal, myCol + jHorizontal);
                 ChessMove nextMove = new ChessMove(position, endPosition, null);
                 if (checkMove(board, nextMove)) {
                     moveList.add(nextMove);
