a8b32913f31b7471159860287ba9ff82ea18fe35
bascom16 <bascomb1@byu.edu>
2025-03-29 17:54:46 -0600

    Created gameplay functions

--- client/src/main/java/client/Gameplay.java
+++ client/src/main/java/client/Gameplay.java
@@ -7,43 +7,34 @@ import state.GameplayState;
 public class Gameplay implements ClientStateInterface {
 
     public String help() {
+        if (gameplayState == GameplayState.OBSERVE) {
+            return """
+               - help (h) | display this help menu
+               - leave (l) | leave the current game
+               - redraw (r) | display the current chess board
+               - highlight (h): <tile> | highlight legal moves for piece at given position
+               """;
+        }
         return """
                - help (h) | display this help menu
-               - quit (q) | logout and exit the Chess program
-               - logout (lo) | logout current user
-               - exit (e) | exit current game
-               - TBD | Have patience, young padawan
+               - leave (l) | leave the current game
+               - redraw (r) | display the current chess board
+               - move (m): [A1-H8] [A1-H8] | move piece from start position to end position
+               - resign (re) | forfeit the game
+               - highlight (hi): <tile> | highlight legal moves for piece at given position
                """;
     }
 
     public String eval(String cmd, String... params) throws ClientException {
         return switch (cmd) {
             case "h", "help" -> help();
-            case "q", "quit" -> quit();
-            case "lo", "logout" -> logout();
-            case "e", "exit" -> exit();
+            case "l", "leave" -> leave();
+            case "r", "redraw" -> draw();
+            case "m", "move" -> makeMove(params);
+            case "re", "resign" -> resign();
+            case "hi", "highlight" -> highlight(params);
             default -> "Command not recognized.\n" + help();
         };
-    }
-
-    private String quit() throws ClientException {
-        logout();
-        return "quit";
-    }
-
-    private String logout() throws ClientException {
-        drawState = GameplayState.WHITE;
-        ChessClient.clearGameDataMap();
-        ChessClient.server.logout(ChessClient.getAuthorization());
-        ChessClient.state = ClientState.PRE_LOGIN;
-        return "Successfully logged out" + ChessClient.help();
-    }
-
-    private String exit() {
-        drawState = GameplayState.WHITE;
-        ChessClient.state = ClientState.POST_LOGIN;
-        ChessClient.setCurrentGameID(0);
-        return "Exited game\n" + ChessClient.help();
     }
 
     private static GameplayState gameplayState = null;
@@ -63,5 +54,31 @@ public class Gameplay implements ClientStateInterface {
 
     public static GameplayState getGameplayState() {
         return gameplayState;
+    }
+
+    private String leave() {
+        throw new RuntimeException("not implemented");
+    }
+
+    public String draw() {
+        throw new RuntimeException("not implemented");
+    }
+
+    private String makeMove(String... params) throws ClientException {
+        if (params.length == 2) {
+            throw new RuntimeException("not implemented");
+        }
+        throw new ClientException(400, "Expected <start position [A1-H8]> <end position [A1-H8]>");
+    }
+
+    private String resign() {
+        throw new RuntimeException("not implemented");
+    }
+
+    private String highlight(String... params) throws ClientException {
+        if (params.length == 1) {
+            throw new RuntimeException("not implemented");
+        }
+        throw new ClientException(400, "Expected piece position [A1-H8]");
     }
 }
