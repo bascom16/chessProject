128ac465e2350def99e809f0d0e6108358a28702
bascom16 <bascomb1@byu.edu>
2025-04-07 19:31:24 -0600

    Added ConnectionManager log messages

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -3,6 +3,8 @@ package server.websocket;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
@@ -10,15 +12,20 @@ import websocket.messages.ServerMessage;
 public class ConnectionManager {
     public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
 
+    Logger log = Logger.getLogger("serverLogger");
+
     public void add(String username, Session session) {
         connections.put(username, new Connection(username, session));
+        log.info(String.format("Added connection for user %s", username));
     }
 
     public void remove(String username) {
         connections.remove(username);
+        log.info(String.format("Removed connection for user %s", username));
     }
 
     public void broadcast(String excludeName, ServerMessage message) throws IOException {
+        log.info(String.format("Broadcasting message [%s]", message));
         ArrayList<Connection> removeList = new ArrayList<>();
         for (Connection connection : connections.values()) {
             if (connection.session.isOpen()) {
@@ -26,6 +33,7 @@ public class ConnectionManager {
                     connection.send(message.toString());
                 }
             } else {
+                log.info(String.format("Connection for user %s is not open", connection.username));
                 removeList.add(connection);
             }
         }
@@ -37,8 +45,10 @@ public class ConnectionManager {
     public void sendToUser(String username, ServerMessage message) throws IOException {
         Connection connection = connections.get(username);
         if (connection == null) {
+            log.info(String.format("Error: Sending message [%s] to user %s failed", message, username));
             throw new IOException("User not found");
         }
+        log.info(String.format("Sending message [%s] to user %s", message, username));
         connection.send(message.toString());
     }
  }
